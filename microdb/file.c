8bb028958aaa24fd3c3e6de64b73a082ba8ab1b1
Koji Yamamoto <kowkow.y@gmail.com>
2016-04-22 16:01:02 +0900

    file module

---
+++ microdb/file.c
@@ -0,0 +1,130 @@
+/*
+ * file.c -- ファイルアクセスモジュール
+ */
+
+#include "microdb.h"
+
+/*
+ * initializeFileModule -- ファイルアクセスモジュールの初期化処理
+ *
+ * 引数:
+ *	なし
+ *
+ * 返り値:
+ *	成功の場合OK、失敗の場合NG
+ */
+Result initializeFileModule()
+{
+    return OK;
+}
+
+/*
+ * finalizeFileModule -- ファイルアクセスモジュールの終了処理
+ *
+ * 引数:
+ *	なし
+ *
+ * 返り値:
+ *	成功の場合OK、失敗の場合NG
+ */
+Result finalizeFileModule()
+{
+    return OK;
+}
+
+/*
+ * createFile -- ファイルの作成
+ *
+ * 引数:
+ *	filename: 作成するファイルのファイル名
+ *
+ * 返り値:
+ *	成功の場合OK、失敗の場合NG
+ */
+Result createFile(char *filename)
+{
+}
+
+/*
+ * deleteFile -- ファイルの削除
+ *
+ * 引数:
+ *	??????
+ *
+ * 返り値:
+ *	??????
+ */
+?????? deleteFile(??????)
+{
+}
+
+/*
+ * openFile -- ファイルのオープン
+ *
+ * 引数:
+ *	??????
+ *
+ * 返り値:
+ *	オープンしたファイルのFile構造体
+ *	オープンに失敗した場合にはNULLを返す
+ */
+File *openFile(??????)
+{
+}
+
+/*
+ * closeFile -- ファイルのクローズ
+ *
+ * 引数:
+ *	クローズするファイルのFile構造体
+ *
+ * 返り値:
+ *	成功の場合OK、失敗の場合NG
+ */
+Result closeFile(File *file)
+{
+}
+
+/*
+ * readPage -- 1ページ分のデータのファイルからの読み出し
+ *
+ * 引数:
+ *	file: アクセスするファイルのFile構造体
+ *	pageNum: 読み出すページの番号
+ *	page: 読み出した内容を格納するPAGE_SIZEバイトの領域
+ *
+ * 返り値:
+ *	成功の場合OK、失敗の場合NG
+ */
+Result readPage(File *file, int pageNum, char *page)
+{
+}
+
+/*
+ * writePage -- 1ページ分のデータのファイルへの書き出し
+ *
+ * 引数:
+ *	file: アクセスするファイルのFile構造体
+ *	pageNum: 書き出すページの番号
+ *	page: 書き出す内容を格納するPAGE_SIZEバイトの領域
+ *
+ * 返り値:
+ *	成功の場合OK、失敗の場合NG
+ */
+Result writePage(File *file, int pageNum, char *page)
+{
+}
+
+/*
+ * getNumPage -- ファイルのページ数の取得
+ *
+ * 引数:
+ *	filename: ファイル名
+ *
+ * 返り値:
+ *	引数で指定されたファイルの大きさ(ページ数)
+ *	エラーの場合には-1を返す
+ */
+int getNumPages(char *filename)
+{
+}
