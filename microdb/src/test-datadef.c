ffdbb28d321c86466b75d23ba0189577495658fa
Koji Yamamoto <kowkow.y@gmail.com>
2016-05-06 17:13:36 +0900

    makefile

---
+++ microdb/src/test-datadef.c
@@ -0,0 +1,18 @@
+/*
+ * ファイルI/Oモジュールテストプログラム
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "../include/microdb.h"
+
+/*
+ * テスト名
+ */
+#define TEST_NAME "test-datadef"
+
+int main(int argc, char const *argv[]) {
+    /* code */
+    return 0;
+}
