2323a27878d0a7b6444e99a4e1d242205727396a
Koji Yamamoto <kowkow.y@gmail.com>
2016-05-06 15:46:20 +0900

    datadef init

---
+++ microdb/src/datadef.c
@@ -0,0 +1,109 @@
+/*
+ * datadef.c - データ定義モジュール
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include "microdb.h"
+
+/*
+ * DEF_FILE_EXT -- データ定義ファイルの拡張子
+ */
+#define DEF_FILE_EXT ".def"
+
+/*
+ * initializeDataDefModule -- データ定義モジュールの初期化
+ *
+ * 引数:
+ *	なし
+ *
+ * 返り値;
+ *	成功ならOK、失敗ならNGを返す
+ */
+Result initializeDataDefModule()
+{
+}
+
+/*
+ * finalizeDataDefModule -- データ定義モジュールの終了処理
+ *
+ * 引数:
+ *	なし
+ *
+ * 返り値;
+ *	成功ならOK、失敗ならNGを返す
+ */
+Result finalizeDataDefModule()
+{
+}
+
+/*
+ * createTable -- 表(テーブル)の作成
+ *
+ * 引数:
+ *	tableName: 作成する表の名前
+ *	tableInfo: データ定義情報
+ *
+ * 返り値:
+ *	成功ならOK、失敗ならNGを返す
+ *
+ * データ定義ファイルの構造(ファイル名: tableName.def)
+ *   +-------------------+----------------------+-------------------+----
+ *   |フィールド数       |フィールド名          |データ型           |
+ *   |(sizeof(int)バイト)|(MAX_FIELD_NAMEバイト)|(sizeof(int)バイト)|
+ *   +-------------------+----------------------+-------------------+----
+ * 以降、フィールド名とデータ型が交互に続く。
+ */
+Result createTable(char *tableName, TableInfo *tableInfo)
+{
+}
+
+/*
+ * dropTable -- 表(テーブル)の削除
+ *
+ * 引数:
+ *	??????
+ *
+ * 返り値:
+ *	??????
+ */
+?????? dropTable(??????)
+{
+}
+
+/*
+ * getTableInfo -- 表のデータ定義情報を取得する関数
+ *
+ * 引数:
+ *	tableName: 情報を表示する表の名前
+ *
+ * 返り値:
+ *	tableNameのデータ定義情報を返す
+ *	エラーの場合には、NULLを返す
+ *
+ * ***注意***
+ *	この関数が返すデータ定義情報を収めたメモリ領域は、不要になったら
+ *	必ずfreeTableInfoで解放すること。
+ */
+TableInfo *getTableInfo(char *tableName)
+{
+}
+
+/*
+ * freeTableInfo -- データ定義情報を収めたメモリ領域の解放
+ *
+ * 引数:
+ *	??????
+ *
+ * 返り値:
+ *	なし
+ *
+ * ***注意***
+ *	関数getTableInfoが返すデータ定義情報を収めたメモリ領域は、
+ *	不要になったら必ずこの関数で解放すること。
+ */
+void freeTableInfo(??????)
+{
+}
